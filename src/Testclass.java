e153d54f406974745518d6f1a7bedcc7932989b8
Rajeev Tewari <tewarirajeev5@gmail.com>
2020-04-07 03:07:58 +0530

    This code is committed on 07/04/2020

---
+++ src/Testclass.java
@@ -0,0 +1,19 @@
+import java.net.MalformedURLException;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class Testclass extends base {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		
+		AndroidDriver<AndroidElement>driver=Capabilities();
+		
+		driver.get("https://www.amazon.in");
+		driver.findElementByXPath("//*[@id=\"twotabsearchtextbox\"]\r\n").sendKeys("i7 processor, 8/16 GB RAM and a screen<=14");
+		driver.findElementByXPath("//*[@id=\"nav-search\"]/form/div[2]/div/input").click();
+
+	}
+
+}
